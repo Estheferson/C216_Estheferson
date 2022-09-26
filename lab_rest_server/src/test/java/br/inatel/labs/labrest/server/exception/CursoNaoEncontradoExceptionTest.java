d5eb0f68a60798d2a767c4e6938851d9c1931180
Estheferson <estheferson01@hotmail.com>
2022-09-25 22:15:31 -0300

    lado servidor

---
+++ lab_rest_server/src/test/java/br/inatel/labs/labrest/server/exception/CursoNaoEncontradoExceptionTest.java
@@ -0,0 +1,14 @@
+package br.inatel.labs.labrest.server.exception;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CursoNaoEncontradoExceptionTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
